a26302ece04c379261e056a81daeef4477d80b29
Dav-Code-Oss <dfordonezpina@gmail.com>
2024-05-07 14:16:54 -0500

    weatherApp developing

---
+++ Projects/WeatherApp/weather.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  var lang = document.documentElement.lang;
+
+  async function getData() {
+    let data = await $.getJSON(`../../langs/WeatherApp/${lang}.json`);
+
+    $("#appTitle").html(data.AppTitle)
+
+    $("#appDesc").html(data.AppDesc)
+    $("#searchISO").html(data.AppSearchBy[0])
+    $("#searchCountry").html(data.AppSearchBy[1])
+    $("#searchCity").html(data.AppSearchBy[2])
+  }
+  getData()
+});
