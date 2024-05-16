3f803d135407dfd02ae591914a0bd050ff27464a
Dav-Code-Oss <dfordonezpina@gmail.com>
2024-05-16 13:52:24 -0500

    bikeshop page ended

--- Projects/appsDesc.js
+++ Projects/appsDesc.js
@@ -6,16 +6,22 @@ $(document).ready(function(){
 
   console.log(url)
   console.log(search)
+  console.log(lang)
 
   async function getData() {
     let data = await $.getJSON(`../langs/${search}/${lang}.json`);
 
     $("#appTitle").html(data.AppTitle)
 
+    $("#appFirstImage").attr("src", data.AppImagesLink[0])
     $("#appDesc").html(data.AppDesc)
+    $("#appSecondImage").attr("src", data.AppImagesLink[1])
     $("#searchISO").html(data.AppSearchBy[0])
+    $("#appThirdImage").attr("src", data.AppImagesLink[2])
     $("#searchCountry").html(data.AppSearchBy[1])
+    $("#appFourthImage").attr("src", data.AppImagesLink[3])
     $("#searchCity").html(data.AppSearchBy[2])
+    $("#appFifthImage").attr("src", data.AppImagesLink[4])
     $("#langButton").html(data.AppSearchBy[3])
     $("#langEffectDesc").html(data.AppSearchBy[4])
   }
